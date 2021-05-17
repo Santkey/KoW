d1f7e960e98c08845bd359cbeb070eded6352499
kacper <kacper.santkiewicz@gmail.com>
2021-05-17 16:33:34 +0200

    23

--- 9o-23.py
+++ 9o-23.py
@@ -1,1 +1,14 @@
 #dekoratory
+
+def decorator(func):
+    def wrapper(): #opakowanie
+        print("-------------")
+        func()
+        print("-------------")
+    return wrapper
+
+def hello():
+    print("Hello World")
+
+hello = decorator(hello)
+hello()
